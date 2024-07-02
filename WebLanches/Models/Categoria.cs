b9a16edceb206bdb574f804c93ba1f3ee5974b31
KaiqueLusvarghi <kaiqueolusvarghi@gmail.com>
2024-07-01 22:38:32 -0300

    modelo de dominio e conexao de string feitas

---
+++ WebLanches/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace WebLanches.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
+
+    //Relacionamento de 1 para muitos (1 -> N)
+    public List<Lanche> Lanches { get; set; }   
+}
---
+++ WebLanches/Models/Lanche.cs
@@ -0,0 +1,18 @@
+﻿namespace WebLanches.Models;
+
+public class Lanche
+{
+    public int LancheId { get; set; }
+    public string Nome { get; set; }
+    public string DescricaoCurta { get; set; }
+    public string DescricaoDetalhada { get; set; }
+    public decimal Preco { get; set; }
+    public string ImagemURL { get; set; }
+    public string ImagemThumbnailURL { get; set;}
+    public bool IslanchePreferido { get; set; }
+    public bool EmEstoque { get; set; }
+
+    //Definindo o relacionamento entre lanche e categoria
+    public int CategoriaId { get; set; }
+    public virtual Categoria Categoria { get; set; }    
+}
