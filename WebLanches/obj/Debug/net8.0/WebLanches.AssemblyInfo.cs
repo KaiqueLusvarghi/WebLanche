b5c531cc0c959ebc218f45e621c0f73c66c0e9df
Kaique Lusvarghi <kaiqueoluvarghi@gmail.com>
2024-11-01 21:37:46 -0300

    Area de Administracao finalizada

--- WebLanches/obj/Debug/net8.0/WebLanches.AssemblyInfo.cs
+++ WebLanches/obj/Debug/net8.0/WebLanches.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebLanches")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efff043cde89f98be700f1a7c37d84be9a32a5b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3cd20afaf558e6704d087e8a7f0aa1e8e8d933b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebLanches")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebLanches")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
